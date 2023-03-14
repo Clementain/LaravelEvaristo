227a553a1af9fd1c3c7b30105925cb9d21eea1a3
Clementain <clentecarmona@gmail.com>
2023-03-14 14:54:36 -0600

    Clase de hoy

---
+++ resources/views/layout/slider.blade.php
@@ -0,0 +1,45 @@
+<!-- Divider -->
+<hr class="sidebar-divider">
+
+<!-- Heading -->
+<div class="sidebar-heading">
+    Interface
+</div>
+
+
+<!-- Divider -->
+<hr class="sidebar-divider">
+
+<!-- Heading -->
+<div class="sidebar-heading">
+    Addons
+</div>
+
+
+<!-- Nav Item - Administración -->
+<li class="nav-item">
+    <a class="nav-link" href="/admin/">
+        <i class="fas fa-fw fa-chart-area"></i>
+        <span>Administración</span></a>
+</li>
+
+<!-- Nav Item - Inventario -->
+<li class="nav-item">
+    <a class="nav-link" href="/inventario/">
+        <i class="fas fa-fw fa-chart-area"></i>
+        <span>Inventario</span></a>
+</li>
+
+<!-- Nav Item - Compras -->
+<li class="nav-item">
+    <a class="nav-link" href="/compras/">
+        <i class="fas fa-fw fa-chart-area"></i>
+        <span>Compras</span></a>
+</li>
+
+<!-- Nav Item - Cerrar Sesión -->
+<li class="nav-item">
+    <a class="nav-link" href="/logout/">
+        <i class="fas fa-fw fa-chart-area"></i>
+        <span>Cerrar Sesión</span></a>
+</li>
